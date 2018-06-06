1ebdfeb36efa36b7f17766d6cf678a7be6597904
user <iago@xxx.com>
2018-06-06 19:30:30 +0200

    implementando factorizacion

--- p1/cholesky.py
+++ p1/cholesky.py
@@ -1,2 +1,21 @@
 import numpy as np #Import module numpy to work with matrix 
+import scipy as sp
+def choleskyFactoring(matrix):
+    dim = np.range(matrix)
+    C = np.zeros((dim,dim),float)
+    
+    
+                
 
+def solveEcuation():
+    pass
+
+def readInput(path):
+    pass
+def writeOutput(path):
+    pass
+
+if __name__ == '__main__':
+    #A = np.loadtxt('p1/xmatrix.txt',float)
+    #solutionM = np.loadtxt()
+    C = np.zeros((2,2),float)  
