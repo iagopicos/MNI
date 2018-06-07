1ba5bbb8fde45dfddb6d3d2252c8bc2a6e08448e
user <iago@xxx.com>
2018-06-07 23:50:46 +0200

    Cholesky factoring implemented and working

--- p1/cholesky.py
+++ p1/cholesky.py
@@ -1,17 +1,24 @@
-from numpy import array, zeros, matrix, linalg, dot, loadtxt, random,ones
+import numpy as np
 import scipy as sp
 import math 
 def choleskyFactoring(A):
     dim = len(A)
-    L = zeros((dim,dim),"f")
+    L = np.zeros((dim,dim),"f")
+    for i in range(dim):
+        for j in range( i + 1):
+            sumatory = sum(L[i,k]*L[j,k] for k in range(dim))
 
+            if i == j:
+                L[i,i] = math.sqrt (A[j,j] - sumatory)
+            else:
+                L[i,j] = 1 / L[j,j] * (A[i,j] - sumatory) 
     return L
                 
 def solveEcuation():
     pass
 
 def readInput(path):
-    M = loadtxt(path)
+    M = np.loadtxt(path,"f")
     return M
 def writeOutput(path):
     pass
@@ -19,7 +26,7 @@ def writeOutput(path):
 if __name__ == '__main__':
     A = readInput('p1/xmatrix.txt')
     C = choleskyFactoring(A)
-    S = linalg.cholesky(A)
+    S = np.linalg.cholesky(A)
 
     print('Solucion Cholesky \n' + str(S))
     print('Mi solucion \n '+str(C))
