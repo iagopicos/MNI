eeacbcac22f236d9b79b31374bea200e6b8f65a1
user <iago@xxx.com>
2018-06-19 18:43:49 +0200

    remain: error and solveCholesky

--- p1/cholesky.py
+++ p1/cholesky.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy as sp
 import math 
+
 def traspuesta(M):
     dim = len(M)
     Mt = np.zeros((dim,dim),"f")
@@ -45,19 +46,30 @@ def gaussSeidel(A,vector,iter,tol,error):
 def methodSOR(A,vector,aproximation,maxIter,maxError,omega):
     dimI = len(A)
     dimJ = len(A[0])
-    x = aproximation
+    #if aproximation == None:
+    x = np.zeros(dimJ,"f")
+    #x = aproximation
     iter = 0 
+    error = 0
     
-    while iter<maxIter:
+    while (iter<maxIter):
         sum = 0
         iter+=1
         for i in range(dimI):
             sum = 0
+            
             for j in range (dimJ):
                 if (i != j): #Comprobamos que no estamos en la diagonal
-                    sum = sum + A[i,j] * x[j]
+                    sum = sum + A[i,j] * x[j] #Preparo la suma de las componentes para luego calcular xi correspondiente
+            anterior = x[i]
             x[i] =  x[i] *(1-omega) +( (vector[i] - sum) * omega / A[i,i])
-    print iter    
+            error = abs((x[i] - anterior) / x[i])
+        if error < maxError:
+                print 'Convergencia alcanzada'
+                break
+            
+    print('Numero de iteraciones: '+str(iter))
+    print('Error final:'+ str(error))    
     return x
 
 
@@ -71,13 +83,16 @@ def readVector(path):
     completePath = 'p1/vector/' + path
     vector = np.loadtxt(completePath)
     return vector
-def writeOutput(path):
+
+def writeSolution(path):
     pass
 
 if __name__ == '__main__':
     A = readMatrix('xmatrix.txt')
     vector = readVector('xvector.txt')
+    
     #print vector
-    #print methodSOR(A,vector,5,0,0,1.2)
-
+    x = methodSOR(A,vector,None,20,0.002,0.6)
+    #(A,vector,aproximation,maxIter,maxError,omega)
+    print x
     
